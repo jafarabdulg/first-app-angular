5e95a93d5c5e74f9afc42c26d6fba52b2e31b360
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-07 16:21:57 +0700

    string interpolation

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,4 +7,32 @@ import {Component} from "@angular/core";
 })
 
 export class HelloComponent{
+  name: string = 'Jafar A G';
+  message: string = 'This is message';
+
+  getMessage(): string {
+    return this.message;
+  }
+
+  people: string[] = ['Fauzan', 'Edy', 'Heru', 'Ibnu'];
+
+  getPeople(): string {
+    let result = '';
+    for (const person of this.people) {
+      result += person + ", ";
+    }
+    result += 'selesai dilooping';
+    return result;
+  }
+
+  employees = [
+    {
+      name: 'Jafar',
+      address: 'Jakarta Selatan'
+    },
+    {
+      name: 'Mel',
+      address: 'Bekasi'
+    }
+  ];
 }
