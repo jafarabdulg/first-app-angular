7dde08e019091ec1679c0d34ba32e15a1c247013
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-08 14:41:03 +0700

    adjustment

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -14,13 +14,12 @@ export class LoginComponent {
   passwordInput: string = '';
 
   auth() {
-    // if (this.emailInput === 'admin@gmail.com' && this.passwordInput === 'admin') {
-    //   alert('login success');
-    //   this.router.navigate(['/dashboard']);
-    // } else {
-    //   alert('login failed');
-    // }
-    this.router.navigate(['dashboard']);
+    if (this.emailInput === 'admin@gmail.com' && this.passwordInput === 'admin') {
+      alert('login success');
+      this.router.navigate(['dashboard']);
+    } else {
+      alert('login failed');
+    }
     console.log(this.emailInput, this.passwordInput);
   }
 }
